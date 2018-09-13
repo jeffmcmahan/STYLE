2d06583960a2a7dc709081d3c746c58a3f2cbe93
Jeff McMahan <jeffrey.lee.mcmahan@gmail.com>
2018-09-13 14:07:15 -0400

    removed nodejs support, reduced thrashing

--- index.js
+++ index.js
@@ -1,50 +1,63 @@
 'use strict'
 
-// Keep track of everything that's been added, and how many times.
-const state = {__string: ''}
+// Keep track of everything that's been added.
+const state = {}
 
 // Inject a <style> block into the DOM.
 function injectCSS(id, styles, media) {
-  if (document.getElementById(id)) return
-  const block = document.createElement('style')
-  block.id = id
-  block.type = 'text/css'
-  block.media = media
-  block.innerHTML = styles
-  document.head.appendChild(block)
+	if (!document.getElementById(id)) {
+		const block = document.createElement('style')
+		block.id = id
+		block.type = 'text/css'
+		block.media = media
+		block.innerHTML = styles
+		document.head.appendChild(block)
+	}
 }
 
 // Remove <style> block from <head>
 function rmCSS(id) {
-  const block = document.getElementById(id)
-  if (!block) return
-  document.head.removeChild(block)
+	const block = document.getElementById(id)
+	if (block) {
+		// Wait 100ms and see whether it still needs removing or not.
+		setTimeout(() => {
+			if (!state[id]) {
+				document.head.removeChild(block)
+			}
+		}, 100)
+	}
 }
 
 // Create a new <style> block (and inject if in a browser).
-function CSS(id, styles, media='screen') {
-  if (!(id in state)) state[id] = 0
-  state[id]++
-  if (state[id]) {
-    if (typeof window === 'undefined') {
-      state.__string += `<style id="${id}" media="${media}">${styles}</style>`
-    } else {
-      injectCSS(id, styles, media)
-    }
-  }
+function CSS(id, styles, media = 'screen') {
+	if (!(id in state)) {
+		state[id] = false
+	}
+	if (!state[id]) {
+		state[id] = true // Track that we have added it.
+		injectCSS(id, styles, media)
+	}
 }
 
 // Decrement the state counter for a given <style> block; remove if sensible.
 CSS.rm = function removeCSS(id) {
-  if (typeof window === 'undefined') {
-    throw new Error('CSS.rm() only applies in the browser.')
-  }
-  if (!(id in state)) return
-  state[id]--
-  if (state[id] <= 0) rmCSS(id)
+	if (!(id in state)) {
+		throw new Error('Style ' + id + ' was never defined.')
+	}
+	if (state[id]) {
+		state[id] = false
+		rmCSS(id)
+	}
 }
 
-CSS.toString = _=> state.__string
-CSS('isolate', '.isolate{border-collapse:separate;border-spacing:0;caption-side:top;cursor:auto;direction:ltr;empty-cells:show;font-family:serif;font-size:medium;font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;line-height:normal;hyphens:none;letter-spacing:normal;list-style:disc outside none;tab-size:8;text-align:left;text-align-last:auto;text-indent:0;text-shadow:none;text-transform:none;visibility:visible;white-space:normal;widows:2;word-spacing:normal;}')
+// Add the .isolate class to the <head>
+CSS('isolate-style', (
+	'.isolate{border-collapse:separate;border-spacing:0;caption-side:top;cursor:auto;'+
+	'direction:ltr;empty-cells:show;font-family:serif;font-size:medium;font-style:normal;'+
+	'font-variant:normal;font-weight:normal;font-stretch:normal;line-height:normal;hyphens:none;'+
+	'letter-spacing:normal;list-style:disc outside none;tab-size:8;text-align:left;'+
+	'text-align-last:auto;text-indent:0;text-shadow:none;text-transform:none;visibility:visible;'+
+	'white-space:normal;widows:2;word-spacing:normal;}'
+))
 
 module.exports = CSS
